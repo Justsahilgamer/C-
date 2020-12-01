4e7cf1462afe02191cc08ea475271d9b7fc00adb
Justsahilgamer <46240899+Justsahilgamer@users.noreply.github.com>
2020-12-01 19:09:03 +0530

    Add files via upload
    there are some errors with this code
    ill solve them soon

---
+++ specific.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a[9],p;
+    int i,top,bot,mid;
+    cout<<"type numbers in ascending= ";
+    for(i=0;i<10;i++)
+    {
+        cin>>a[i];
+    }
+    top=0;
+    bot=9;
+    cout<<"number to find= ";
+    cin>>p;
+    mid=(top+bot)/2;
+    while ((top<=bot)&&(a[mid]!=p))
+    {
+        if (p,a[mid])
+        bot=mid-1;
+        else
+        top=mid+1;
+        mid=(top+bot)/2;
+    }
+    if(a[mid]==p)
+    
+        cout<<"the number is at position "<<(mid+1);
+    else
+       cout<<"the number didn't found= ";
+}
