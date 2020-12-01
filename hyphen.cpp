90e3eb99ae7d868bfbb9ea8295024906a2acdd44
Justsahilgamer <46240899+Justsahilgamer@users.noreply.github.com>
2020-12-01 18:15:47 +0530

    Add files via upload
    this is a c++ program which applies "-"(hyphen) between the space of you sentence
    example- what-is-your-name?

---
+++ hyphen.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[80];
+    int i,j;
+    cout<<"enter the sentence=";
+    cin.get(str,80);
+    for ( i = 0;str[i]!='\0'; i++)
+    {
+       if(str[i]==' ')
+       {
+           str[i]='-';
+       }
+    }
+    cout<<"sentence with hyphen is=";
+    for(j=0;str[j]!='\0';j++)
+    {
+        cout<<str[j];
+    }
+}
