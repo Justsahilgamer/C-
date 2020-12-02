c723dd3bcd74aeb55540fbdfc2da8340e3252014
Justsahilgamer <46240899+Justsahilgamer@users.noreply.github.com>
2020-12-02 17:46:48 +0530

    Add files via upload

---
+++ Fahrenheit.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class temperature
+{
+    public:
+    float f,c,i;
+    void farenheit();
+};
+void temperature::farenheit()
+{
+    cout<<" enter the temperature in celsius = ";
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<<" value of temperature in farenheit is "<<f<<"\n";
+}
+int main()
+{
+    temperature t;
+    t.farenheit();
+}
